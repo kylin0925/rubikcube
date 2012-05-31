81dda60db2ca6c9989958b0bbe9a82a6830a65b0
kunyi.lin <kylin25@gmail.com>
2012-05-31 23:07:54 +0800

    day 0531

--- rubik.py
+++ rubik.py
@@ -6,7 +6,36 @@ rubik={ 'b':[['b','b'],['b','b']],
         'u':[['u','u'],['u','u']],
         'd':[['d','d'],['d','d']]}
 rblv = 2
-print rubik['u']
+#clockwise
+def h2v_copy(array1,hy,array2,vx):
+    for y in range(rblv):
+        array2[y][vx]=array1[hy][y]
+
+def v2h_copy(array1,vx,array2,vy):
+    for x in range(rblv):
+        array2[vy][x]=array1[(rblv-1)-x][vx]
+
+def v2v_copy(array1,hy,array2,vx):
+    for y in range(rblv):
+        array2[rblv-1-y][vx]=array1[y][vx]
+
+
+#counter clockwise
+def h2vcw_copy(array1,hy,array2,vx):
+    for y in range(rblv):
+        array2[rblv-1-y][vx]=array1[hy][y]
+
+def v2hcw_copy(array1,vx,array2,vy):
+    for x in range(rblv):
+        array2[vy][rblv-1-x]=array1[x][vx]
+
+def v2vcw_copy(array1,hy,array2,vx):
+    for y in range(rblv):
+        array2[rblv-1-y][vx]=array1[y][vx]
+
+
+#---------------------------------------------    
+
 def rote(face,layer,direction):
     global rubik
     tmp = None
@@ -27,11 +56,37 @@ def rote(face,layer,direction):
             rubik['f'][0] = copy.copy(rubik['l'][0])
             rubik['l'][0] = copy.copy(tmp)
 
-    #elif face == 'r':
+    #elif face == 'l':
     #elif face == 'f':
-rote('u',0,'right');
-print rubik
-print "-----------------"
-rote('u',0,'left');
+#rote('u',0,'right');
+#print rubik
+#print "-----------------"
+#rote('u',0,'left');
+#
+#print rubik
+test=[[1,1],[2,3]]
+test2=[[0,0],[0,0]]
+h2v_copy(test,1,test2,0)
+print test2
+test=[[1,1],[2,3]]
+test2=[[0,0],[0,0]]
+v2h_copy(test,0,test2,1)
+print test2
+test=[[1,1],[2,3]]
+test2=[[0,0],[0,0]]
+v2v_copy(test,0,test2,0)
+print test2
 
-print rubik
+test=[[1,1],[2,3]]
+test2=[[0,0],[0,0]]
+h2vcw_copy(test,1,test2,0)
+print test2
+test=[[1,1],[2,3]]
+test2=[[0,0],[0,0]]
+v2hcw_copy(test,0,test2,1)
+print test2
+test=[[1,1],[2,3]]
+test2=[[0,0],[0,0]]
+v2vcw_copy(test,0,test2,0)
+print test2
+
