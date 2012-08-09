d79311698f01aa5811df86dce3a2a0770eb33b7f
kunyi.lin <kylin25@gmail.com>
2012-08-09 23:26:00 +0800

    add rubik cube draw function

--- rubik.py
+++ rubik.py
@@ -15,6 +15,40 @@ rubik={ 'b':[['b','b'],['b','b']],
         'u':[['u','u'],['u','u']],
         'd':[['d','d'],['d','d']]}
 rblv = 2
+#draw black
+red = u'\x1b[%dm\u2587\x1b[0m' % 31
+green = u'\x1b[%dm\u2587\x1b[0m' % 32
+yellow = u'\x1b[%dm\u2587\x1b[0m' % 33
+blue = u'\x1b[%dm\u2587\x1b[0m' % 34
+white = u'\x1b[%dm\u2587\x1b[0m' % 37
+magenta = u'\x1b[%dm\u2587\x1b[0m' % 35
+black = u'\x1b[%dm\u2587\x1b[0m' % 30
+color_map= {'l':red,'f':green,'u':yellow,'d':white,'r':magenta,'b':blue}
+
+def black_str(n):
+    return "".join([black for i in range(n)])
+
+def color_blk(face,lv,n):
+    tmp = ""
+    if face !='l':
+        for i in range(n):
+            tmp = tmp + color_map[rubik[face][lv][i]]
+    else:
+        for i in range(n-1,-1,-1):
+            tmp = tmp + color_map[rubik[face][lv][i]]
+    return tmp
+
+def draw_cube(rblv):
+    n = rblv
+    for i in range(2):
+        print black_str(n)+color_blk('u',i,n)
+
+    for i in range(2):
+        print color_blk('l',i,n)+color_blk('f',i,n)+color_blk('r',i,n)+ color_blk('b',i,n)
+
+    for i in range(2):
+        print black_str(n)+color_blk('d',0,n)
+
 #clockwise
 def raw_copy(array1,y1,x1,array2,y2,x2):
     for idx in range(rblv):
@@ -73,12 +107,11 @@ def rotate(face,layer,direction):
             raw_copy(rubik['d'],slayer,"idx",rubik['l'],"idx","rblv-1" + "-" + slayer)
             raw_copy(rubik['r'],"idx","rblv-1" + "-" + slayer,rubik['d'],slayer,"idx")
             raw_copy(tmp,slayer,"idx",rubik['r'],"idx","rblv-1" + "-" + slayer)
-def draw_cube():
-    i =0
 #test
-rotate('u',0,'right')
+rotate('u',0,'left')
+rotate('u',1,'left')
 print rubik['b']
 print rubik['l'],rubik['u'],rubik['r']
 print rubik['f']
 print rubik['d']
-
+draw_cube(rblv)
